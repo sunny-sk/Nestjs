c4b49aee2ce17ed54e73cddecd54b304c7551207
sunny-sk <sk956223@gmail.com>
2021-01-27 23:55:08 +0530

    signup api added

---
+++ src/auth/dto/auth-credentials.dto.ts
@@ -0,0 +1,16 @@
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
+export class AuthCredentialsDto {
+  @IsString()
+  @MaxLength(20)
+  @MinLength(4)
+  username: string;
+
+  @IsString()
+  @MaxLength(20)
+  @MinLength(4)
+  @Matches(/((?=.*\w+))(?=.*\w+)(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'password is to week',
+  })
+  password: string;
+}
--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,5 +1,18 @@
 import { Repository, EntityRepository } from 'typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
 
 @EntityRepository(User)
-export class UserRepository extends Repository<User> {}
+export class UserRepository extends Repository<User> {
+  async signup(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+    const user = new User();
+    user.username = username;
+    user.password = password;
+    try {
+      await user.save();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
