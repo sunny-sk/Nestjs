e9e8d6ac3918cde7e377f634e8e4bef795d6f93e
sunny-sk <sk956223@gmail.com>
2021-02-11 19:54:42 +0530

    more roles added

--- src/constants/constant.ts
+++ src/constants/constant.ts
@@ -2,4 +2,12 @@ export const SECRET = 'THISISMYSECRET';
 export enum ROLE {
   User = 'user',
   Admin = 'admin',
+  TALENTAQ = 'talentAquasition',
+  INTERVIEWER = 'interviewer',
 }
+
+export const SCHEMA = {
+  user: 'User',
+  category: 'Category',
+  task: 'Tasks',
+};
